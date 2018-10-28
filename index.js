0bcb2a4633e97ebc5a2118c7e7fa3497734de3fb
domitille.andrieu <domitille.andrieu@gmail.com>
2018-10-28 13:04:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout (string){
+  return string.toUpperCase()
+}
+
+function whisper (string){
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma (string){
+  var lowercase = "string"
+  lowercase.toLowerCase() === lowercase
+  return "I can't hear you"
+  console.log("I can't hear you") }
