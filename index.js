26c812f9a3f697e86302a7c4830fc29e63185d28
domitille.andrieu <domitille.andrieu@gmail.com>
2018-10-28 13:34:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ function logWhisper (string){
 }
 
 
-function sayHiToGrandma(string){
-  if ((string) === lowercase){
+
+function sayHiToGrandma(string) {
+  if ((string)===uppercase) {
+    return 'YES INDEED!'
+  }
+  else if ((string)===lowercase) {
     return "I can't hear you!"
   }
-  else if ((string) === uppercase){
-    return "YES INDEED!"
-  }
-  else if ((string) === "I love you, Grandma.")) {
+  else if ((string)==="I love you, Grandma.")) {
     return "I love you, too."
   }
-}
+  }
